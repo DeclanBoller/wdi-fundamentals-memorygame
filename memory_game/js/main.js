78f24c789a5a1dae895b48e57ff827c3578b25ac
Declan Boller <declan.boller@hotmail.com>
2018-11-25 15:49:41 +1100

    added further game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImg: "images/king-of-hearts.png",
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImg: "images/king-of-diamonds.png",
+  },
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImg: "images/queen-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImg: "images/queen-of-diamonds.png",
+  },
+];
+
 var cardsInPlay = [];
 
 function checkMatch() {
@@ -12,8 +34,10 @@ function checkMatch() {
 }
 
 function flipCard(cardId) {
-  console.log(`User flipped ${cards[cardId]}`);
-  cardsInPlay.push(cards[cardId]);
+  console.log(`User flipped ${cards[cardId].rank}`);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImg);
   checkMatch();
 }
 
